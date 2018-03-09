e40b85da81c8289e72b0f6acab1f16c3aa199e4b
ajith3333 <35687948+ajith3333@users.noreply.github.com>
2018-03-09 15:52:31 +0530

    Create 84.c

---
+++ 84.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+clrscr();
+{
+ char ch;
+ printf("\nenter the character:");
+ scanf("%c",ch);
+ printf("the value of character is %c=%d",ch,ch);
+ getch();
+ return 0;
+ }
